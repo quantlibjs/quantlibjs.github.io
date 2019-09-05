b5ca05b163d0469ee07a30531e65453dd6e5ddab
Jerry <jerry.yang.jin@outlook.com>
2019-09-05 14:07:42 +0800

    update get started

--- get_started.mjs
+++ get_started.mjs
@@ -1,27 +1,10 @@
 describe('get started', () => { 
-    it('  ', () => { 
-        expect(true).toEqual(true);
-    }); 
-    it('quantlib.js is javascript version of quantlib', () => { 
+    it(
+        'quantlib.js is javascript version of quantlib\n'+
+        'try selecting a test or example from top menu\n'+
+        'spec will be displayed on left panel\n'+
+        'result will be displayed on right panel', () => { 
       expect(true).toEqual(true);
     }); 
-    it('  ', () => { 
-        expect(true).toEqual(true);
-    }); 
-    it('try selecting a test or example from top menu', () => { 
-        expect(true).toEqual(true);
-    }); 
-    it('  ', () => { 
-        expect(true).toEqual(true);
-    }); 
-    it('spec will be displayed on left panel', () => { 
-        expect(true).toEqual(true);
-    });
-    it('  ', () => { 
-        expect(true).toEqual(true);
-    }); 
-    it('result will be displayed on right panel', () => { 
-        expect(true).toEqual(true);
-    });
 });
   
