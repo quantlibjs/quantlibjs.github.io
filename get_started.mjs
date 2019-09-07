af21313a7acbb8f7359bb2610889ea6fa65ae2de
Jerry <jerry.yang.jin@outlook.com>
2019-09-07 11:11:23 +0800

    update meta

---
+++ get_started.mjs
@@ -0,0 +1,17 @@
+describe('get started', () => { 
+    it('quantlib.js is javascript version of quantlib', () => { 
+      expect(true).toEqual(true);
+    });
+
+    it('try selecting a test or example from top menu', () => { 
+      expect(true).toEqual(true);
+    });
+
+    it('spec will be displayed on left panel', () => { 
+      expect(true).toEqual(true);
+    }); 
+
+    it('result will be displayed on right panel', () => { 
+     expect(true).toEqual(true);
+    }); 
+});
