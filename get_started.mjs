d098d1c273bc4c2284fb16de23263ab8115c060f
Jerry <jerry.yang.jin@outlook.com>
2019-09-12 11:14:06 +0800

    move misc stuff to cdn

--- get_started.mjs
+++
@@ -1,6 +0,0 @@
-describe('get started', () => { 
-    print('quantlib.js is javascript version of quantlib')
-    print('try selecting a test or example from top menu');
-    print('spec will be displayed on left panel'); 
-    print('result will be displayed on right panel'); 
-});
